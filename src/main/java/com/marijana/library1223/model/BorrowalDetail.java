ccf7ff52ff9e3e5af4526514932cd71e4522ca73
marijana82 <marijana.mikolcic@gmail.com>
2023-11-28 21:27:58 +0100

    refactored BorrowalDetails into BorrowalDetail and filled with fields, getters, setters, and annotations

--- src/main/java/com/marijana/library1223/model/BorrowalDetail.java
+++ src/main/java/com/marijana/library1223/model/BorrowalDetail.java
@@ -1,4 +1,22 @@
 package com.marijana.library1223.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="borrowal_details")
 public class BorrowalDetail {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private LocalDate borrowalDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
+
+    //private Status status (take from enumeration);
+
 }
