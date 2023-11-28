b7b9627803bc624ff284bd2c32642a3f153d831c
marijana82 <marijana.mikolcic@gmail.com>
2023-11-28 16:51:02 +0100

    configured database in application.properties

---
+++ src/main/java/com/marijana/library1223/Library1223Application.java
@@ -0,0 +1,13 @@
+package com.marijana.library1223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Library1223Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Library1223Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/marijana/library1223/Library1223ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marijana.library1223;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Library1223ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
