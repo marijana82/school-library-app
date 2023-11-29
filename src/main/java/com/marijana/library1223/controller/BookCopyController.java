acc36e9b6a1b5272a360014644bde2fb54ba07e5
marijana82 <marijana.mikolcic@gmail.com>
2023-11-29 11:04:30 +0100

    added RestController annotation to all controller classes

--- src/main/java/com/marijana/library1223/controller/BookCopyController.java
+++ src/main/java/com/marijana/library1223/controller/BookCopyController.java
@@ -1,4 +1,9 @@
 package com.marijana.library1223.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book-copies")
 public class BookCopyController {
 }
--- src/main/java/com/marijana/library1223/controller/PictureBookController.java
+++ src/main/java/com/marijana/library1223/controller/PictureBookController.java
@@ -1,4 +1,7 @@
 package com.marijana.library1223.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PictureBookController {
 }
