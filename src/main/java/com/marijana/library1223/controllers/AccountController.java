7a216c951368b113fb4840b4a4838d8a19b22e21
marijana82 <marijana.mikolcic@gmail.com>
2023-11-30 21:50:06 +0100

    AccountService added to AccountController via constructor injection

--- src/main/java/com/marijana/library1223/controllers/AccountController.java
+++ src/main/java/com/marijana/library1223/controllers/AccountController.java
@@ -1,9 +1,17 @@
 package com.marijana.library1223.controllers;
 
+import com.marijana.library1223.services.AccountService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+        
+    }
 }
