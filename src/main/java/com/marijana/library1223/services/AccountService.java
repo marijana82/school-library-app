dabedb50c1c132dd8bc389fa3ed7e87dde4dd6b1
marijana82 <marijana.mikolcic@gmail.com>
2023-11-30 21:35:38 +0100

    added service annotation and constructor injection to AccountService class

--- src/main/java/com/marijana/library1223/services/AccountService.java
+++ src/main/java/com/marijana/library1223/services/AccountService.java
@@ -1,4 +1,16 @@
 package com.marijana.library1223.services;
 
+import com.marijana.library1223.repositories.AccountRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+
 }
