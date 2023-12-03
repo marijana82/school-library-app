2609cc039b56527e95e0d3b2103b67ca5ab533b6
marijana82 <marijana.mikolcic@gmail.com>
2023-12-03 14:55:08 +0100

    inserted values into accounts table in data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+--Account.....................................
+INSERT INTO accounts (id, first_name_student, last_name_student, dob, student_class, name_of_teacher)
+VALUES (1000, 'Ana', 'Anx', '2018-01-01', 'obb', 'Gaby'),
+       (1001, 'Anita', 'Binx', '2017-02-02', 'obb', 'Gaby'),
+       (1002, 'Bram', 'Brenner', '2016-03-04', 'mba', 'Chrystel'),
+       (1003, 'Ivan', 'Ivanovic', '2016-04-04', 'mba', 'Chrystel'),
+       (1004, 'Ilon', 'Verhoef', '2015-05-05', 'mbc', 'Tina'),
+       (1005, 'Ravi', 'Verhoef', '2015-08-08', 'mbe', 'Dennis'),
+       (1006, 'Daan', 'Dam', '2014-01-01', 'bba', 'Mirjam'),
+       (1007, 'Abel', 'Rijn', '2014-02-02', 'bba', 'Mirjam'),
+       (1008, 'Teuntje', 'Paulinus', '2013-07-07', 'bbc', 'Ricardo'),
+       (1009, 'Lu', 'Manfreda', '2012-09-09', 'bbd', 'Max');
+
+
+
