9f7b477e9b036eae933ddbf2a9ce3891a59dcb35
marijana82 <marijana.mikolcic@gmail.com>
2023-12-06 15:06:36 +0100

    constructor njection for pictureBookService created in PictureBookController class

--- src/main/java/com/marijana/library1223/controllers/PictureBookController.java
+++ src/main/java/com/marijana/library1223/controllers/PictureBookController.java
@@ -1,7 +1,14 @@
 package com.marijana.library1223.controllers;
 
+import com.marijana.library1223.services.PictureBookService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PictureBookController {
+
+    private final PictureBookService pictureBookService;
+
+    public PictureBookController(PictureBookService pictureBookService) {
+        this.pictureBookService = pictureBookService;
+    }
 }
--- src/main/java/com/marijana/library1223/dtos/PictureBookDto.java
+++ src/main/java/com/marijana/library1223/dtos/PictureBookDto.java
@@ -1,4 +1,13 @@
 package com.marijana.library1223.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class PictureBookDto {
+    private Long id;
+    private String type;
+    @NotBlank(message = "Please provide name of the illustrator.")
+    private String nameIllustrator;
+
 }
