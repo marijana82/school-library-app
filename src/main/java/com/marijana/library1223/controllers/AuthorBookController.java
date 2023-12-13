ea6e0f765c9a28b8537adba3f3952377c1c1edd4
marijana82 <marijana.mikolcic@gmail.com>
2023-12-13 16:51:35 +0100

    added AuthorBookService to AuthorBookController via dependency injection

---
+++ src/main/java/com/marijana/library1223/controllers/AuthorBookController.java
@@ -0,0 +1,20 @@
+package com.marijana.library1223.controllers;
+
+//only used for the "in between class" AuthorBook
+//it only contains post-method to add author to book
+//no get method because this is only backend implementation, 
+// user is not aware of its existence
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/author-book")
+public class AuthorBookController {
+
+    private final AuthorBookService authorBookService;
+
+    public AuthorBookController(AuthorBookService authorBookService) {
+        this.authorBookService = authorBookService;
+    }
+}
