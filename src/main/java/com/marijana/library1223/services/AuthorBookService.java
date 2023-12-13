abb9ec310bf5c66835decace103605c03e675e0f
marijana82 <marijana.mikolcic@gmail.com>
2023-12-13 18:09:47 +0100

    getBooksByIdAuthor method created in AuthorBookService class

--- src/main/java/com/marijana/library1223/services/AuthorBookService.java
+++ src/main/java/com/marijana/library1223/services/AuthorBookService.java
@@ -1,6 +1,7 @@
 package com.marijana.library1223.services;
 
 import com.marijana.library1223.dtos.AuthorDto;
+import com.marijana.library1223.dtos.BookDto;
 import com.marijana.library1223.exceptions.RecordNotFoundException;
 import com.marijana.library1223.models.Author;
 import com.marijana.library1223.models.AuthorBook;
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 //this class contains the methods of AuthorBookController
 //this class is different than the other service classes, because this one is autowired in 3 different controllers
@@ -43,6 +46,30 @@ public class AuthorBookService {
             authorDtos.add(authorDto);
         }
         return authorDtos;
+    }
+
+    //get BooksByIdAuthor
+    public Collection<BookDto> getBooksByIdAuthor(Long idAuthor) {
+        //set to avoid double entries
+        Set<BookDto> bookSetDtos = new HashSet<>();
+        List<AuthorBook> authorBookList = authorBookRepository.findAllByAuthorId(idAuthor);
+        for(AuthorBook authorBook : authorBookList) {
+            Book book = authorBook.getBook();
+            BookDto bookDto = new BookDto();
+            bookDto.setBookTitle(book.getBookTitle());
+            bookDto.setId(book.getId());
+            bookDto.setNameAuthor(book.getNameAuthor());
+            bookDto.setIsbn(book.getIsbn());
+            bookDto.setNameIllustrator(book.getNameIllustrator());
+            bookDto.setSuitableAge(book.getSuitableAge());
+            bookDto.setInformationBook(book.getInformationBook());
+            bookDto.setReadingBook(book.getReadingBook());
+            //bookDto.setReservationDto(book.getReservation());
+            //bookDto.setReservationId();
+            //bookDto.setBookCopyList(book.getBookCopyList());
+            bookSetDtos.add(bookDto);
+        }
+        return bookSetDtos;
     }
 
     //addAuthorBook
