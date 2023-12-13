6b7dc6efdb2a8a3908c36dff323741fa69758519
marijana82 <marijana.mikolcic@gmail.com>
2023-12-13 16:42:41 +0100

    renamed values of MapsId annotations to match authorId and bookId in fields in AuthorBookKey class

--- src/main/java/com/marijana/library1223/models/AuthorBook.java
+++ src/main/java/com/marijana/library1223/models/AuthorBook.java
@@ -7,23 +7,23 @@ import lombok.Data;
 @Entity
 public class AuthorBook {
 
-    //embedded id - so that there's no new id created
+    //embedded id - so that there's no new id created, use AuthorBookKey id
     @EmbeddedId
-    private AuthorBook id;
+    private AuthorBookKey id;
 
     //this is the owner of the relation
     //there's a foreign key in the database
     //why sometimes Lazy and sometimes Eager?
     @ManyToOne(fetch = FetchType.LAZY)
     //@MapsId makes it happen that the variables get combined to a key?
-    @MapsId
+    @MapsId("authorId")
     @JoinColumn(name = "author_id")
     private Author author;
 
     //this is the owner of the relation
     //there's a foreign key in the database
     @ManyToOne
-    @MapsId("book_id")
+    @MapsId("bookId")
     @JoinColumn(name = "book_id")
     private Book book;
 }
