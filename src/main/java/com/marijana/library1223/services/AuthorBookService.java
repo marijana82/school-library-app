e335a40eaeb72c195f68d53c298db7e5bc682ccf
marijana82 <marijana.mikolcic@gmail.com>
2023-12-13 17:42:27 +0100

    getAuthorsByIdBook method created in AuthorBookService

--- src/main/java/com/marijana/library1223/services/AuthorBookService.java
+++ src/main/java/com/marijana/library1223/services/AuthorBookService.java
@@ -1,9 +1,15 @@
 package com.marijana.library1223.services;
 
+import com.marijana.library1223.dtos.AuthorDto;
+import com.marijana.library1223.models.Author;
+import com.marijana.library1223.models.AuthorBook;
 import com.marijana.library1223.repositories.AuthorBookRepository;
 import com.marijana.library1223.repositories.AuthorRepository;
 import com.marijana.library1223.repositories.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashSet;
 
 //this class contains the methods of AuthorBookController
 //this class is different than the other service classes, because this one is autowired in 3 different controllers
@@ -20,6 +26,24 @@ public class AuthorBookService {
         this.bookRepository = bookRepository;
         this.authorBookRepository = authorBookRepository;
     }
+
+    //getAuthorsByBookId
+    public Collection<AuthorDto> getAuthorsByIdBook(Long idBook) {
+        Collection<AuthorDto> authorDtos = new HashSet<>();
+        Collection<AuthorBook> authorBooks = authorBookRepository.findAllByIdBook(idBook);
+        for(AuthorBook authorBook : authorBooks) {
+            Author author = authorBook.getAuthor();
+            AuthorDto authorDto = new AuthorDto();
+            authorDto.setCountry(author.getCountry());
+            authorDto.setId(author.getId());
+            authorDto.setName(author.getName());
+            authorDtos.add(authorDto);
+        }
+        return authorDtos;
+
+
+    }
+
 
 
 
