d58810d37ac27111041a29bdc6e0c339b5dbd45a
marijana82 <marijana.mikolcic@gmail.com>
2023-12-13 18:25:59 +0100

    get mapping method called getBooksByIdAuthor created in AuthorController

--- src/main/java/com/marijana/library1223/controllers/AuthorController.java
+++ src/main/java/com/marijana/library1223/controllers/AuthorController.java
@@ -1,7 +1,13 @@
 package com.marijana.library1223.controllers;
 
+import com.marijana.library1223.dtos.BookDto;
 import com.marijana.library1223.services.AuthorBookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController("/authors")
 public class AuthorController {
@@ -11,5 +17,11 @@ public class AuthorController {
 
     public AuthorController(AuthorBookService authorBookService) {
         this.authorBookService = authorBookService;
+    }
+
+    //method to get all books connected to a certain author
+    @GetMapping("/books/{idAuthor}")
+    public ResponseEntity<Collection<BookDto>> getBooksByIdAuthor(@PathVariable("idAuthor") Long idAuthor) {
+        return ResponseEntity.ok(authorBookService.getBooksByIdAuthor(idAuthor));
     }
 }
