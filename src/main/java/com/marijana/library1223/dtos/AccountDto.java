5bde873107d6d8d3c57e8a5985062bd3105c4c5b
marijana82 <marijana.mikolcic@gmail.com>
2023-12-18 11:50:08 +0100

    inserted Data lombok in AccountDto and deleted all getters and setters

--- src/main/java/com/marijana/library1223/dtos/AccountDto.java
+++ src/main/java/com/marijana/library1223/dtos/AccountDto.java
@@ -4,9 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 import java.time.LocalDate;
-
+@Data
 public class AccountDto {
 
     private Long id;
@@ -26,52 +27,4 @@ public class AccountDto {
     //by writing in the json the field: "reservationIds": 1 - for example
     public Long[] reservationIds;
 
-    //getters & setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstNameStudent() {
-        return firstNameStudent;
-    }
-
-    public void setFirstNameStudent(String firstNameStudent) {
-        this.firstNameStudent = firstNameStudent;
-    }
-
-    public String getLastNameStudent() {
-        return lastNameStudent;
-    }
-
-    public void setLastNameStudent(String lastNameStudent) {
-        this.lastNameStudent = lastNameStudent;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public String getStudentClass() {
-        return studentClass;
-    }
-
-    public void setStudentClass(String studentClass) {
-        this.studentClass = studentClass;
-    }
-
-    public String getNameOfTeacher() {
-        return nameOfTeacher;
-    }
-
-    public void setNameOfTeacher(String nameOfTeacher) {
-        this.nameOfTeacher = nameOfTeacher;
-    }
 }
