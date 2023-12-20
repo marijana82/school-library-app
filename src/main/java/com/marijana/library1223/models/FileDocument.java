dd91100941711c90c2b5fcddfdcd9d9b22ee00e6
marijana82 <marijana.mikolcic@gmail.com>
2023-12-20 14:52:13 +0100

    created FileUpload entity in Models package

--- src/main/java/com/marijana/library1223/models/FileDocument.java
+++ src/main/java/com/marijana/library1223/models/FileDocument.java
@@ -1,4 +1,18 @@
 package com.marijana.library1223.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "file_uploads")
 public class FileDocument {
+
+    @Id
+    private Long id;
+    private String fileName;
+    private String contentType;
+    private String url;
 }
