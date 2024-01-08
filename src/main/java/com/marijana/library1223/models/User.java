9e57893ed05a6b7299e876d0643cb7eb42390b1c
marijana82 <marijana.mikolcic@gmail.com>
2024-01-08 09:40:04 +0100

    added @Id annotation to username to create primary key

--- src/main/java/com/marijana/library1223/models/User.java
+++ src/main/java/com/marijana/library1223/models/User.java
@@ -1,11 +1,14 @@
 package com.marijana.library1223.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
+
 public class User {
+    @Id
     private String username;
     private String password;
     private boolean enabled = true;
