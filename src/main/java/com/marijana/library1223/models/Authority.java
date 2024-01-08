28b7344ca4c58c746cc1de5e991aa274d5ec3302
marijana82 <marijana.mikolcic@gmail.com>
2024-01-08 09:58:39 +0100

    created new class Authority with fields username and authority, and added @Id annotation above both fields

---
+++ src/main/java/com/marijana/library1223/models/Authority.java
@@ -0,0 +1,25 @@
+package com.marijana.library1223.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+//@IdClass(AuthorityKey.class)
+@Table(name = "authorities")
+public class Authority implements Serializable {
+    @Id
+    @Column(nullable = false)
+    private String username;
+
+    @Id
+    @Column(nullable = false)
+    private String authority;
+
+
+}
