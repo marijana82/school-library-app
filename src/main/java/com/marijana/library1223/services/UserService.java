1ada6f5307233cd2adeeaafbe491a1c6f128bae7
marijana82 <marijana.mikolcic@gmail.com>
2024-01-08 10:44:05 +0100

    created methods in UserService getUsers and getUser

--- src/main/java/com/marijana/library1223/services/UserService.java
+++ src/main/java/com/marijana/library1223/services/UserService.java
@@ -1,7 +1,14 @@
 package com.marijana.library1223.services;
 
+import com.marijana.library1223.dtos.UserDto;
+import com.marijana.library1223.exceptions.UsernameNotFoundException;
+import com.marijana.library1223.models.User;
 import com.marijana.library1223.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,6 +20,36 @@ public class UserService {
         this.userRepository = userRepository;
 
     }
+
+    //get all users
+    public List<UserDto> getUsers() {
+        List<UserDto> collection = new ArrayList<>();
+        List<User> list = userRepository.findAll();
+        for (User user : list) {
+            collection.add(fromUser(user));
+        }
+        return collection;
+    }
+
+    //get one user
+    public UserDto getUser(String username) {
+        UserDto dto = new UserDto();
+        Optional<User> user = userRepository.findById(username);
+        if (user.isPresent()){
+            dto = fromUser(user.get());
+        }else {
+            throw new UsernameNotFoundException(username);
+        }
+        return dto;
+    }
+
+
+    //user exists
+    public boolean userExists(String username) {
+        return userRepository.existsById(username);
+    }
+
+
 
 
 }
