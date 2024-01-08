405a4f435fc671f83f7075390d4014f0e42ccc99
marijana82 <marijana.mikolcic@gmail.com>
2024-01-08 11:11:33 +0100

    created filter map and added JwtRequestFilter class to it

---
+++ src/main/java/com/marijana/library1223/filter/JwtRequestFilter.java
@@ -0,0 +1,9 @@
+package com.marijana.library1223.filter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtRequestFilter {
+
+    //add the code after implementing dependencies
+}
