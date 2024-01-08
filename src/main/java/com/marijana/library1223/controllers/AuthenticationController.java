01c5bc403103cf7425880ac6a7890f0c7401135d
marijana82 <marijana.mikolcic@gmail.com>
2024-01-08 11:02:34 +0100

    created AuthenticationController

---
+++ src/main/java/com/marijana/library1223/controllers/AuthenticationController.java
@@ -0,0 +1,9 @@
+package com.marijana.library1223.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class AuthenticationController {
+}
