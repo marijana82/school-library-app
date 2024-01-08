e6a21343f58da68e73056362da49f6a7e728e909
marijana82 <marijana.mikolcic@gmail.com>
2024-01-08 21:01:00 +0100

    secret key and getSignInKey method created in JwtUtil class

--- src/main/java/com/marijana/library1223/utils/JwtUtil.java
+++ src/main/java/com/marijana/library1223/utils/JwtUtil.java
@@ -1,8 +1,20 @@
 package com.marijana.library1223.utils;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtUtil {
+
+    //secret key, min 45 characters
+    private final static String SECRET_KEY = "trebanamtakomaloskoronistajedankremenijednaiskradatamaodezauvijekizsna";
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
 }
