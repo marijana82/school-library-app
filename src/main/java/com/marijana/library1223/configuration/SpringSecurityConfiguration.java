acb2ae69bcffd37d6f7f8223411c88093140ef99
marijana82 <marijana.mikolcic@gmail.com>
2024-01-09 09:58:12 +0100

    injected customUserDetailService and jwtRequestFilter in SpringSecurityConfiguration

--- src/main/java/com/marijana/library1223/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/marijana/library1223/configuration/SpringSecurityConfiguration.java
@@ -1,4 +1,21 @@
 package com.marijana.library1223.configuration;
 
+import com.marijana.library1223.filter.JwtRequestFilter;
+import com.marijana.library1223.services.CustomUserDetailsService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfiguration {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
+
+    public SpringSecurityConfiguration(CustomUserDetailsService customUserDetailsService, JwtRequestFilter jwtRequestFilter) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+
 }
