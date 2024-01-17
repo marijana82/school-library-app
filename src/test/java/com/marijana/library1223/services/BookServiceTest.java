15f80595139d5791441ceaedbfd6ec3b084fd1f8
marijana82 <marijana.mikolcic@gmail.com>
2024-01-17 11:07:07 +0100

    generated tests of BookService methods

---
+++ src/test/java/com/marijana/library1223/services/BookServiceTest.java
@@ -0,0 +1,94 @@
+package com.marijana.library1223.services;
+
+import com.marijana.library1223.repositories.BookRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class BookServiceTest {
+
+    @Mock
+    BookRepository bookRepository;
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @Disabled
+    void createNewBook() {
+    }
+
+    @Test
+    @Disabled
+    void showOneBook() {
+    }
+
+    @Test
+    @Disabled
+    void showAllBooks() {
+    }
+
+    @Test
+    @Disabled
+    void showAllBooksByNameAuthor() {
+    }
+
+    @Test
+    @Disabled
+    void showAllBooksByNameIllustrator() {
+    }
+
+    @Test
+    @Disabled
+    void showAllBooksByNameIllustratorAndNameAuthor() {
+    }
+
+    @Test
+    @Disabled
+    void showAllBooksByTopic() {
+    }
+
+    @Test
+    @Disabled
+    void deleteById() {
+    }
+
+    @Test
+    @Disabled
+    void updateOneBook() {
+    }
+
+    @Test
+    @Disabled
+    void updateBookPartially() {
+    }
+
+    @Test
+    @Disabled
+    void transferBookToBookDto() {
+    }
+
+    @Test
+    @Disabled
+    void transferBookDtoToBook() {
+    }
+
+    @Test
+    @Disabled
+    void transferBookToInformationBookDto() {
+    }
+}
