72e63bffc504e972eb5a60d53f18acbfdbe54d28
marijana82 <marijana.mikolcic@gmail.com>
2024-01-18 10:36:21 +0100

    successfully performed tests for showAllBooksByNameIllustrator and showAllBooksByNameIllustratorAndNameAuthor methods

--- src/test/java/com/marijana/library1223/services/BookServiceTest.java
+++ src/test/java/com/marijana/library1223/services/BookServiceTest.java
@@ -59,6 +59,7 @@ class BookServiceTest {
     void setUp() {
         book1 = new Book(1000L, 98765, "Kleine onderzoekers voertuigen", "Ruth Martin", "Ruth Martin", 4);
         book2 = new Book(1001L, 8765, "Graafmachines en kiepautos", "Angela Royston", "David Barrow", 4);
+        book3 = new Book(1002L, 763987, "Barbapapa", "Annette Tilson", "Talus Taylor", 5 );
     }
 
     //defining that the database gets cleaned up
@@ -75,33 +76,26 @@ class BookServiceTest {
 
     @Test
     @DisplayName("Should show one book")
-    //@Disabled
     void showOneBook() {
         //Arrange
         when(bookRepository.findById(1000L)).thenReturn(Optional.of(book1));
-
         //Act
         BookDto bookDto = bookService.showOneBook(1000L);
-
         //Assert
         assertEquals(book1.getBookTitle(), bookDto.getBookTitle());
-
         //Assert in case the book is not found
         assertThrows(RecordNotFoundException.class, () -> {
-            bookService.showOneBook(1002L);
+            bookService.showOneBook(null);
         });
     }
 
     @Test
     @DisplayName("Should show all books")
-    //@Disabled
     void showAllBooks() {
         //Arrange
         when(bookRepository.findAll()).thenReturn(List.of(book1, book2));
         //Act
-        //List<Book> booksFound = bookRepository.findAll();
         List<BookDto> bookDtoList = bookService.showAllBooks();
-
         //Assert
         assertEquals(book1.getBookTitle(), bookDtoList.get(0).getBookTitle());
         assertEquals(book2.getBookTitle(), bookDtoList.get(1).getBookTitle());
@@ -110,28 +104,48 @@ class BookServiceTest {
 
     @Test
     @DisplayName("Should show all books by name author")
-    @Disabled
     void showAllBooksByNameAuthor() {
         //Arrange
+        when(bookRepository.findAllBooksByNameAuthorEqualsIgnoreCase("Ruth Martin")).thenReturn(List.of(book1));
+        //Act
+        List<BookDto> bookDtoList = bookService.showAllBooksByNameAuthor("Ruth Martin");
+        //Assert
+        assertEquals(book1.getNameAuthor(), bookDtoList.get(0).getNameAuthor());
 
+    }
+
+    @Test
+    @DisplayName("Should show all books by name illustrator")
+    void showAllBooksByNameIllustrator() {
+        //Arrange
+        when(bookRepository.findAllBooksByNameIllustratorEqualsIgnoreCase("David Barrow")).thenReturn(List.of(book2));
+        //Act
+        List<BookDto> bookDtoList = bookService.showAllBooksByNameIllustrator("David Barrow");
+        //Assert
+        assertEquals(book2.getNameIllustrator(), bookDtoList.get(0).getNameIllustrator());
+    }
+
+    @Test
+    @DisplayName("Should show all books by name illustrator and name author")
+    void showAllBooksByNameIllustratorAndNameAuthor() {
+        //Arrange
+        when(bookRepository.findAllBooksByNameIllustratorAndNameAuthorEqualsIgnoreCase("Talus Taylor", "Annette Tilson")).thenReturn(List.of(book3));
+        //Act
+        List<BookDto> bookDtoList = bookService.showAllBooksByNameIllustratorAndNameAuthor("Talus Taylor", "Annette Tilson");
+        //Assert
+        assertEquals(book3.getNameIllustrator(), bookDtoList.get(0).getNameIllustrator());
+        assertEquals(book3.getNameAuthor(), bookDtoList.get(0).getNameAuthor());
+
+    }
+
+    @Test
+    @DisplayName("Should show all books by topic")
+    @Disabled
+    void showAllBooksByTopic() {
+        //Arrange
         //Act
         //Assert
 
-    }
-
-    @Test
-    @Disabled
-    void showAllBooksByNameIllustrator() {
-    }
-
-    @Test
-    @Disabled
-    void showAllBooksByNameIllustratorAndNameAuthor() {
-    }
-
-    @Test
-    @Disabled
-    void showAllBooksByTopic() {
     }
 
     @Test
