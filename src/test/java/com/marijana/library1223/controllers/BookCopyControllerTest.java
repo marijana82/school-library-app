eda7449032f5db857e16749a36cacc0d12408732
marijana82 <marijana.mikolcic@gmail.com>
2024-01-22 11:18:45 +0100

    imported @SpringBootTest, @AutoconfigureMockMvc, and @MockBean annotations in BookCopyControllerTest

--- src/test/java/com/marijana/library1223/controllers/BookCopyControllerTest.java
+++ src/test/java/com/marijana/library1223/controllers/BookCopyControllerTest.java
@@ -1,11 +1,26 @@
 package com.marijana.library1223.controllers;
 
+import com.marijana.library1223.services.BookCopyService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class BookCopyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookCopyService bookCopyService;
+
 
     @BeforeEach
     void setUp() {
