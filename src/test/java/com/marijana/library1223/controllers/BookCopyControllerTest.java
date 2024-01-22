14b2706f96ec4d3d7551bf909b0c6f02932df507
marijana82 <marijana.mikolcic@gmail.com>
2024-01-22 11:37:32 +0100

    imported @Autowired BookService and BookRepository

--- src/test/java/com/marijana/library1223/controllers/BookCopyControllerTest.java
+++ src/test/java/com/marijana/library1223/controllers/BookCopyControllerTest.java
@@ -1,7 +1,11 @@
 package com.marijana.library1223.controllers;
 
+import com.marijana.library1223.dtos.BookCopyDto;
+import com.marijana.library1223.models.Book;
 import com.marijana.library1223.models.BookCopy;
+import com.marijana.library1223.repositories.BookRepository;
 import com.marijana.library1223.services.BookCopyService;
+import com.marijana.library1223.services.BookService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,10 +28,23 @@ class BookCopyControllerTest {
     @MockBean
     private BookCopyService bookCopyService;
 
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    BookCopyDto bookCopyDto1;
+    BookCopyDto bookCopyDto2;
+    BookCopyDto bookCopyDto3;
 
     BookCopy bookCopy1;
     BookCopy bookCopy2;
     BookCopy bookCopy3;
+
+    Book book1;
+    Book book2;
+    Book book3;
 
     @BeforeEach
     public void setUp() {
@@ -40,6 +57,7 @@ class BookCopyControllerTest {
         bookCopy1.setNumberOfPages(30);
         bookCopy1.setTotalWordCount(300);
         bookCopy1.setYearPublished(LocalDate.of(2020, 01, 01));
+        bookCopy1.setBook(book1);
 
         bookCopy2.setId(2L);
         bookCopy2.setBarcode(23456);
@@ -50,6 +68,7 @@ class BookCopyControllerTest {
         bookCopy2.setNumberOfPages(80);
         bookCopy2.setTotalWordCount(8000);
         bookCopy2.setYearPublished(LocalDate.of(2021, 01, 01));
+        bookCopy2.setBook(book2);
 
         bookCopy3.setId(3L);
         bookCopy3.setBarcode(34567);
@@ -60,6 +79,7 @@ class BookCopyControllerTest {
         bookCopy3.setNumberOfPages(28);
         bookCopy3.setTotalWordCount(100);
         bookCopy3.setYearPublished(LocalDate.of(2022, 01, 01));
+        bookCopy3.setBook(book3);
     }
 
     @Test
